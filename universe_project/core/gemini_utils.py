7b7a30fd0d8f316acd0c901f866c86ad06701f2a
shamanshsg@gmail.com <shamanshsg@gmail.com>
2025-11-30 18:15:42 +0300

    trr

--- universe_project/core/gemini_utils.py
+++ universe_project/core/gemini_utils.py
@@ -1,5 +1,6 @@
 # core/gemini_utils.py
 import google.generativeai as genai
+from google.generativeai.types import HarmCategory, HarmBlockThreshold
 from django.conf import settings
 
 genai.configure(api_key=settings.GEMINI_API_KEY)
@@ -12,8 +13,17 @@ generation_config = {
   "max_output_tokens": 2048,
 }
 
+safety_settings = {
+    HarmCategory.HARM_CATEGORY_HARASSMENT: HarmBlockThreshold.BLOCK_NONE,
+    HarmCategory.HARM_CATEGORY_HATE_SPEECH: HarmBlockThreshold.BLOCK_NONE,
+    HarmCategory.HARM_CATEGORY_SEXUALLY_EXPLICIT: HarmBlockThreshold.BLOCK_NONE,
+    HarmCategory.HARM_CATEGORY_DANGEROUS_CONTENT: HarmBlockThreshold.BLOCK_NONE,
+}
+
+
 model = genai.GenerativeModel(model_name="gemini-2.5-flash",
-                              generation_config=generation_config)
+                              generation_config=generation_config,
+                              safety_settings=safety_settings)
 
 def generate_content(prompt_text):
 
