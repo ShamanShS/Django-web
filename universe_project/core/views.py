d6b907ed61ae1958e6d9119601d034a2f2498f2f
shamanshsg@gmail.com <shamanshsg@gmail.com>
2025-12-02 22:05:30 +0300

    fix

--- universe_project/core/views.py
+++ universe_project/core/views.py
@@ -1,5 +1,5 @@
 # core/views.py
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect,  get_object_or_404
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth import login
 from django.contrib.auth.decorators import login_required
